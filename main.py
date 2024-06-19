d34f4ff7b826cf3c00bdabe74f44f810c0d04d47
Chanho Lee <ibear6954@gmail.com>
2024-06-19 09:45:30 +0900

    Improve variables, add multiplication

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import numpy as np
 from variable import Variable
-from function import square, exp, add
+from function import square, exp, add, mul
 from config import no_grad
 
 
 def main():
-    x0 = Variable(np.array(1.0))
-    x1 = Variable(np.array(1.0))
-    t = add(x0, x1)
-    y = add(x0, t)
+    a = Variable(np.array(3.0))
+    b = Variable(np.array(2.0))
+    c = Variable(np.array(1.0))
+
+    y = add(mul(a, b), c)
+
     y.backward()
-    print(y.grad, t.grad)  # None None
-    print(x0.grad, x1.grad)  # 2.0 1.0
 
-    with no_grad():
-        x = Variable(np.array(2.0))
-        y = square(x)
-
-    print(x.data, y.data)
+    print(f'y = {y.data}, a.grad = {a.grad}, b.grad = {b.grad}')
 
 if __name__ == '__main__':
     main()
